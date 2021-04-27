a6809730c7caa5b741d848793928fccf38bc9d65
MarwanReyadAL-Khawaldeh <marwanreyad@gmail.com>
2021-04-27 14:03:03 +0300

    final

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 
 const client = new pg.Client({ connectionString: process.env.DATABASE_URL,
-    //  ssl: { 
-    //      rejectUnauthorized: false }
+     ssl: { 
+         rejectUnauthorized: false }
 
          });
 
